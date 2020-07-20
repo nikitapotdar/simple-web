a0d5554cd9cc17415d32b503cf3df04f78f6081f
Nikita Potdar <51308022+nikitapotdar@users.noreply.github.com>
2020-07-20 15:47:43 +0530

    simple web file

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+
+import ("fmt"
+		"net/http"
+
+)  
+
+func index_handler(w http.ResponseWriter,r *http.Request){
+	fmt.Fprintf(w,"wowwww , she is so cute")
+}
+func about_handler(w http.ResponseWriter,r *http.Request){
+	fmt.Fprintf(w,"great you are selected")
+}
+func main(){
+	http.HandleFunc("/" ,index_handler)
+	http.HandleFunc("/about/" ,about_handler)
+	http.ListenAndServe(":8000" ,nil)
+}
